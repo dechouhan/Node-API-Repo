b422040dbcad29d9a43a5421adf2310ae48ec032
Devendra <dechouhan@gmail.com>
2021-08-23 14:37:43 +0530

    Assignment is updated and use of request.query work is completed

--- src/routers/taskRouter.js
+++ src/routers/taskRouter.js
@@ -2,7 +2,6 @@ const express = require("express");
 const Tasks = require("../models/taskModel");
 const routerTasks = new express.Router();
 
-
 routerTasks.post("/tasks", async (req, res) => {
   try {
     const addingTask = new Tasks(req.body);
@@ -15,42 +14,21 @@ routerTasks.post("/tasks", async (req, res) => {
 
 routerTasks.get("/tasks", async (req, res) => {
   try {
-    const allTasksData = await Tasks.find({});
-    res.send(allTasksData);
+    if (req.query.startDate && req.query.endDate) {
+      const allMembersData = await Tasks.find({
+        $and: [
+          { date: { $gte: req.query.startDate } },
+          { date: { $lte: req.query.endDate } },
+        ],
+      });
+      res.send(allMembersData);
+    } else {
+      const allMembersData = await Tasks.find({});
+      res.send(allMembersData);
+    }
   } catch (e) {
     res.status(404).send(e);
   }
 });
-routerTasks.get("/tasks/:username", async (req, res) => {
-  try {
-    var regex =await new RegExp(req.params.username,'i');
-    const allMembersData = await Tasks.find({username:regex});
-    res.send(allMembersData);
-  } catch (e) {
-    res.status(404).send(e);
-  }
-});
-
-// routerTasks.patch("/users/:id", async (req, res) => {
-//   try {
-//     const _id = req.params.id;
-//     const selectedUsersData = await Users.findByIdAndUpdate(_id, req.body, {
-//       new: true,
-//     });
-//     res.send(selectedUsersData);
-//   } catch (e) {
-//     res.status(404).send(e);
-//   }
-// });
-
-// routerTasks.delete("/users/:id", async (req, res) => {
-//   try {
-//     const _id = req.params.id;
-//     const selectedUsersData = await Users.findByIdAndDelete(_id);
-//     res.send(selectedUsersData);
-//   } catch (e) {
-//     res.status(404).send(e);
-//   }
-// });
 
 module.exports = routerTasks;
